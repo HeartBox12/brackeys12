f3290f0ad60f835630e3f6d846050819320f69ef
HeartBox12 <maximosity1@gmail.com>
2024-09-08 12:03:00 -0700

    Workaround for the camera

--- brackeytower/Level/level.gd
+++ brackeytower/Level/level.gd
@@ -12,6 +12,10 @@ func _ready(): #To begin, stack three sections.
 	lastSection = place_section($firstPos.position)
 	lastSection = place_section(lastSection.next_pos())
 	lastSection = place_section(lastSection.next_pos())
+	
+	$camera.position_smoothing_enabled = false
+	$camera.position.y = $player.position.y
+	$camera.position_smoothing_enabled = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- brackeytower/Level/sections/section.gd
+++ brackeytower/Level/sections/section.gd
@@ -1,7 +1,14 @@
 extends Node2D
 
+var onScreen:bool = false
+
+func _on_screen_entered():
+	onScreen = true
+
+
 func _on_screen_exited():
-	queue_free()
+	if onScreen:
+		queue_free()
 
 func next_pos():
 	return $nextSpot.global_position
