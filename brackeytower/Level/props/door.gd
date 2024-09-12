bda947fbafaea6a73aa64e27125f1391dc0e5606
HeartBox12 <maximosity1@gmail.com>
2024-09-12 09:55:05 -0700

    Implemented pickup sprite and door opening sound

--- brackeytower/Level/props/door.gd
+++ brackeytower/Level/props/door.gd
@@ -1,7 +1,11 @@
 extends StaticBody2D
 
+func _ready():
+	$sound.volume_db = Global.volume_db
+
 func open():
 	$sprite.play("open")
+	$sound.play()
 
 func _on_animation_finished():
 	collision_layer = 0
