8db661d402504973803b4e54c75a6de327b0de8e
HeartBox12 <maximosity1@gmail.com>
2024-09-14 09:32:32 -0700

    Put in a volume slider

--- brackeytower/Menu/menu.gd
+++ brackeytower/Menu/menu.gd
@@ -4,3 +4,6 @@ signal start
 
 func _on_start_pressed():
 	start.emit()
+
+func _on_vol_slider_changed():
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear_to_db($volSlider.value))
